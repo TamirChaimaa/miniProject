606b897426b6d91c12d52741bcf5ae08eaf036cf
TamirChaimaa <chaimaatamir43@gmail.com>
2023-03-10 17:10:25 +0100

    first change

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -7,5 +7,7 @@ import { Component, OnInit } from "@angular/core";
 export class LoginComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    alert("da7iya")
+  }
 }

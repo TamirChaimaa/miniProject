80baa1d7d3450624ff02d41c2e64b96a405f0b33
younessSofware <youness3.toyouness18@gmail.com>
2023-03-28 22:16:18 +0000

    fix merge

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -4,16 +4,10 @@ import { Component, OnInit } from "@angular/core";
   selector: "app-login",
   templateUrl: "./login.component.html",
 })
-export class LoginComponent  {
+export class LoginComponent implements OnInit {
   constructor() {}
 
-<<<<<<< HEAD
   ngOnInit(): void {
     alert("da7iya youness")
-    console.log("Youness Dassar" )
-=======
-  test(){
-    console.log("youness abaa3");
->>>>>>> e89dd122f34995cd64903ef493b449d37eda5b93
   }
 }
